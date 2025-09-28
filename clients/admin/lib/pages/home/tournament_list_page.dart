ab593dc1d68a88cdd993c9732d4be49c245edcd9
shoheisasaki <sasa.tankytank@gmail.com>
2025-09-28 21:06:37 +0900

    refactor: トーナメント関連のクラス名を変更し、依存関係を整理
    - TournamentクラスをMockTournamentにリネームし、モックデータの明確化
    - 不要なインポートを削除し、コードの可読性を向上
    - _export.dartでのエクスポート順序を整理し、依存関係を明確化
    - injection.dartでのエクスポートを整理し、重複を解消
    
    これにより、コードの整合性と可読性が向上しました。

--- clients/admin/lib/pages/home/tournament_list_page.dart
+++ clients/admin/lib/pages/home/tournament_list_page.dart
@@ -1,5 +1,4 @@
 import 'package:base_ui/base_ui.dart';
-import 'package:domain/domain.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
