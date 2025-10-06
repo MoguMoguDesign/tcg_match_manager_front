da6c65b0f1da69eba81c96089ae9295a81dde3ae
shoheisasaki <sasa.tankytank@gmail.com>
2025-10-06 23:38:23 +0900

    feat: RestoreSessionUseCaseのプロバイダーを追加し、モッククラスを生成
    - `restore_session_use_case.g.dart`: RestoreSessionUseCaseのプロバイダーを追加
    - `restore_session_use_case_test.mocks.dart`: ValidatePlayerUseCaseのモッククラスを生成
    
    ドキュメントのサブモジュール参照も更新。

---
+++ packages/domain/lib/src/player/restore_session_use_case.g.dart
@@ -0,0 +1,32 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'restore_session_use_case.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$restoreSessionUseCaseHash() =>
+    r'e7a34a68035438db74e3823563c41f7e2e6b9b94';
+
+/// [RestoreSessionUseCase] を提供する。
+///
+/// Copied from [restoreSessionUseCase].
+@ProviderFor(restoreSessionUseCase)
+final restoreSessionUseCaseProvider =
+    AutoDisposeProvider<RestoreSessionUseCase>.internal(
+      restoreSessionUseCase,
+      name: r'restoreSessionUseCaseProvider',
+      debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+          ? null
+          : _$restoreSessionUseCaseHash,
+      dependencies: null,
+      allTransitiveDependencies: null,
+    );
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef RestoreSessionUseCaseRef =
+    AutoDisposeProviderRef<RestoreSessionUseCase>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
